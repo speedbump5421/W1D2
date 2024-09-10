4ceff65d1a5db8ed273e67bc55150b67340e8e52
Daniel Morales <155024431+speedbump5421@users.noreply.github.com>
2024-09-10 11:47:11 -0500

    Add files via upload

---
+++ cc1.py
@@ -0,0 +1,20 @@
+# Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a))
+
+b = 5.5
+print(type(b))
+
+c = "duck"
+print(type(c))
+
+d = a + b
+print(d)
+
+
+
+
+
